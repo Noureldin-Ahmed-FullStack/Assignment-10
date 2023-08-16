4d954548267ee4e02cb63490e2d5edd8392f2559
Nour Eldin <noureldin2662002@gmail.com>
2023-08-16 16:09:29 +0300

    test2

--- MyScripts.js
+++ MyScripts.js
@@ -89,7 +89,7 @@ function SignUpFunc() {
 
                 }
                 if (flag == true) {
-                  console.log(MyData)
+                    console.log(MyData)
                     MyData.push(myObj)
                     localStorage.setItem('Accounts', JSON.stringify(MyData))
                     document.getElementById("ErrorArea").innerHTML = `<p class = "text-success">Email registered successully!</p>`
